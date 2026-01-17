156cd446ecc6eaa826aa76a25c1dd2e02e8dc1c2
Shema Alphonse <ashema330@gmail.com>
2026-01-17 23:44:04 +0200

    initial commit

---
+++ Hospital-managment-system_Spring/src/main/java/com/shema/Hospital_managment_system_Spring/HospitalManagmentSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shema.Hospital_managment_system_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagmentSystemSpringApplication.class, args);
+	}
+
+}
---
+++ Hospital-managment-system_Spring/src/test/java/com/shema/Hospital_managment_system_Spring/HospitalManagmentSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shema.Hospital_managment_system_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagmentSystemSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
