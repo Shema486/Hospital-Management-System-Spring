f9ed0ed22cf323aad0370e6404c1897671dbc1ab
Shema Alphonse <ashema330@gmail.com>
2026-01-28 18:55:44 +0200

    refactor(graphql): fixing bugs in the inventory query

--- Hospital-managment-system_Spring/src/main/java/com/shema/Hospital_managment_system_Spring/graphqlController/InventoryGraphqlController.java
+++ Hospital-managment-system_Spring/src/main/java/com/shema/Hospital_managment_system_Spring/graphqlController/InventoryGraphqlController.java
@@ -8,40 +8,39 @@ import lombok.AllArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @Controller
 @AllArgsConstructor
 public class InventoryGraphqlController {
-    private MedicalInventoryService medicalInventoryService;
+
+    private final MedicalInventoryService medicalInventoryService;
 
     @QueryMapping
-    public List<MedicalInventoryResponseDTO> inventories(){
+    public List<MedicalInventoryResponseDTO> inventories() {
         return medicalInventoryService.getAllInventoryItems();
     }
+
     @MutationMapping
     public MedicalInventoryResponseDTO addInventory(
-            @Valid @Argument MedicalInventoryRequestDTO requestDTO){
-        return medicalInventoryService.addInventoryItem(requestDTO);
+            @Valid @Argument MedicalInventoryRequestDTO input) {
+        return medicalInventoryService.addInventoryItem(input);
     }
+
     @MutationMapping
     public MedicalInventoryResponseDTO updateInventory(
             @Argument Long id,
-            @Valid @Argument("input") MedicalInventoryRequestDTO requestDTO){
-        return medicalInventoryService.updateInventoryItem(id,requestDTO);
-    }
-    @MutationMapping
-    public String deleteInventory(@PathVariable Long id){
-        medicalInventoryService.deleteInventoryItem(id);
-        return "Inventory deleted successfully" + id;
+            @Valid @Argument MedicalInventoryRequestDTO input) {
+        return medicalInventoryService.updateInventoryItem(id, input);
     }
 
+    @MutationMapping
+    public String deleteInventory(@Argument Long id) {
+        medicalInventoryService.deleteInventoryItem(id);
+        return "Inventory deleted successfully with id: " + id;
+    }
 }
+
