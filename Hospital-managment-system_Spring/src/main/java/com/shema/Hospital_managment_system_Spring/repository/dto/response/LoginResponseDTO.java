3d970cc80518fe730201086d06f7c414ed7bc0f5
Shema Alphonse <ashema330@gmail.com>
2026-01-28 15:14:49 +0200

    feat(auth): add login response DTO with user info

---
+++ Hospital-managment-system_Spring/src/main/java/com/shema/Hospital_managment_system_Spring/repository/dto/response/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.shema.Hospital_managment_system_Spring.repository.dto.response;
+
+import com.shema.Hospital_managment_system_Spring.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String username;
+    private Role role;
+    private String fullName;
+}
